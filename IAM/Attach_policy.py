45ba22cfa1547e89a00ea882191eb6450be48052
Uday <70462083+Uday599@users.noreply.github.com>
2023-07-16 12:15:36 +0530

    Update Attach_policy.py

--- IAM/Attach_policy.py
+++ IAM/Attach_policy.py
@@ -13,3 +13,8 @@ def attach_policy(policy_arn, username):
 
 
 attach_policy('arn:aws:iam::aws:policy/AmazonRDSFullAccess', 'testuser')
+
+
+###
+{'ResponseMetadata': {'RequestId': '0767c989-12a9-4de3-bfd4-39b2c07bfbf8', 'HTTPStatusCode': 200, 'HTTPHeaders': {'x-amzn-requestid': '0767c989-12a9-4de3-bfd4-39b2c07bfbf8', 
+'content-type': 'text/xml', 'content-length': '212', 'date': 'Sun, 16 Jul 2023 06:44:49 GMT'}, 'RetryAttempts': 0}}

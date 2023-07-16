94d0ad529178f10dac2d52445ba27193ae0e65ec
Uday <70462083+Uday599@users.noreply.github.com>
2023-07-16 11:54:18 +0530

    list_user.py

---
+++ IAM/list_user.py
@@ -0,0 +1,25 @@
+import boto3
+
+
+def all_users():
+    iam = boto3.client('iam')
+
+    paginator = iam.get_paginator('list_users')
+
+    for response in paginator.paginate():
+        for user in response['Users']:
+            username = user['UserName']
+            Arn = user['Arn']
+            print('Username : {} Arn : {}'.format(username, Arn))
+
+
+all_users()
+
+
+##################################################################################
+
+Username : aws-python Arn : arn:aws:iam::528418585832:user/aws-python
+Username : itadmin-IAM Arn : arn:aws:iam::528418585832:user/itadmin-IAM
+Username : testuser Arn : arn:aws:iam::528418585832:user/testuser
+Username : testuser2 Arn : arn:aws:iam::528418585832:user/testuser2
+Username : vprofile-s3-admin Arn : arn:aws:iam::528418585832:user/vprofile-s3-admin

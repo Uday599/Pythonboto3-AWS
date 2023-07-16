998b5ce34f8a26281107c2e11fac56efb7c994ea
Uday <70462083+Uday599@users.noreply.github.com>
2023-07-16 12:12:24 +0530

    Create list_policy.py

---
+++ IAM/list_policy.py
@@ -0,0 +1,24 @@
+# All this code we can get in boto3 docs
+
+import boto3
+
+
+def list_policies():
+    iam = boto3.client('iam')
+
+    paginator = iam.get_paginator('list_policies')
+
+    for response in paginator.paginate(Scope="AWS"):
+        for policy in response['Policies']:
+            policy_name = policy['PolicyName']
+            Arn = policy['Arn']
+
+            print('Policy Name : {} Arn : {}'.format(policy_name, Arn))
+
+
+list_policies()
+
+
+######
+
+Huge list

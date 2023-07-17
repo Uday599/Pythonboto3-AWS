0960df5cfdd826b3bac6a5ec1aebbfa50c5a4a81
Uday <70462083+Uday599@users.noreply.github.com>
2023-07-17 20:11:26 +0530

    Update EC2.py

--- EC2.py
+++ EC2.py
@@ -194,3 +194,36 @@ response = ec2_client.delete_key_pair(
 )
 
 print(response)
+
+
+## 13. Attach User data 
+
+import boto3
+
+
+user_data_script = """
+#!/bin/bash
+yum update -y 
+yum install -y httpd
+chkconfig httpd on
+service httpd start
+echo "<h1>Welcome to Python & Boto3 Course</h1>" > /var/www/html/index.html
+
+"""
+
+ec2_resource = boto3.resource('ec2')
+
+response = ec2_resource.create_instances(
+    ImageId='ami-0b0dcb5067f052a63',
+    MinCount= 1,
+    MaxCount  =1,
+    InstanceType='t2.micro',
+    KeyName='mykey',
+    SecurityGroups=[
+        'launch-wizard-1'
+    ],
+    UserData = user_data_script
+)
+
+print(response)
+
